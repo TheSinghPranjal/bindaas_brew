f6d1cfe9a2d5ad3423ba056a0850bbd3b411141f
Pranjal Singh <pranjal.singh@informa.com>
2026-03-25 17:57:41 +0530

    product provider

--- lib/shared_folder/providers/products_provider.dart
+++ lib/shared_folder/providers/products_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 class ProductsNotifier extends StateNotifier<List<Map<String, dynamic>>> {
   ProductsNotifier()
